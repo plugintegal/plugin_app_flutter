0347945b8dc9da807f4d17d5dc725f4b9c006815
ydhnwb <yudhanewbie@gmail.com>
2019-12-23 13:41:17 +0700

    create person page

--- lib/pages/person_page.dart
+++ lib/pages/person_page.dart
@@ -14,23 +14,84 @@ class PersonPage extends StatefulWidget {
 
 class _PersonPageState extends State<PersonPage> implements PersonView{
   PersonPresenter _presenter;
+  ScrollController _scrollController;
+  bool _isCollapsed = false;
+  String _title = "";
 
   @override
   void initState() {
     super.initState();
     _presenter = PersonPresenter(this);
+    _scrollController = ScrollController();
+    _scrollController.addListener((){
+      if (_scrollController.offset > 220 && !_scrollController.position.outOfRange) {
+          if(!_isCollapsed){
+            _title = this.widget.person.name;
+            _isCollapsed = true;
+            setState(() {});
+          }
+       }
+       if (_scrollController.offset <= 220 && !_scrollController.position.outOfRange) {
+         if(_isCollapsed){
+            _title = "";
+            _isCollapsed = false;
+            setState(() {});
+         }
+       }
+    });
   }
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Profile"),
-      ),
-      body: Column(
-        children: <Widget>[
-          Text(this.widget.person.name),
-          Text(this.widget.person.memberId)
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 280.0,
+            floating: false,
+            elevation: 2.0,
+            backgroundColor: Colors.deepOrange,
+            pinned: true,
+            title: Text(_title),
+            centerTitle: true,
+            flexibleSpace: FlexibleSpaceBar(
+              collapseMode: CollapseMode.parallax,
+              centerTitle: true,
+              background: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 46.0,
+                    backgroundImage: NetworkImage("https://ih1.redbubble.net/image.526556262.0001/flat,1000x1000,075,f.u1.jpg"),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 16.0),
+                    child: Text(this.widget.person.name, style: TextStyle(
+                      fontSize: 18.0, color: Colors.white, fontWeight: FontWeight.bold
+                    ),
+                    ),
+                  ),
+                  Container(
+                    child: Text(this.widget.person.email, style: TextStyle(
+                      fontSize: 16.0, color: Colors.white
+                    ),
+                  ),
+                  )
+                ]
+              ),
+            )
+          ),
+          SliverFillRemaining(
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Text("AAA")
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
